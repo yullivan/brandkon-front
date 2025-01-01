45eba6c08bfc94051a0caf33537adf134434a62f
Yul Jeon <dalbitstream@gmail.com>
2025-01-01 14:12:12 +0900

    chore: 인기 상품 스타일링

--- src/app/[category]/page.tsx
+++ src/app/[category]/page.tsx
@@ -58,11 +58,19 @@ export default async function Page({
         {products.map((product, index) => (
           <div key={index} className="flex gap-2">
             <div>
-              <img src="/placeholder.jpg" alt="" width={100} height={100} />
+              <img
+                src="/placeholder.jpg"
+                alt=""
+                width={100}
+                height={100}
+                className="rounded-lg"
+              />
             </div>
-            <div className="flex flex-col">
-              <div>{index + 1}위</div>
-              <div>
+            <div className="flex flex-col justify-center">
+              <div className="text-sm text-blue-500 font-semibold">
+                {index + 1}위
+              </div>
+              <div className="text-sm">
                 {product.brandName} | {product.productName}
               </div>
               <div className="text-lg font-semibold">
